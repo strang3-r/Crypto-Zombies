7f8d9df1ed4e788cd60a5e4efd38b94fed4c7833
strang3-r <singhatmagyan@outlook.com>
2023-06-01 20:11:08 +0530

    Crypto-Zombies

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { HiMenuAlt4 } from 'react-icons/hi';
-import { AiOutlineClose } from 'react-icons/Ai';
+import { AiOutlineClose } from 'react-icons/ai';
 
 import logo from '../../images/logo.png';
 
